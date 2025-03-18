a975bd0b84145678da4190124c1d2c0bb2fea1c4
R0moo <roditto123@gmail.com>
2025-03-18 08:54:02 -0300

    aula 2 compras

--- compras.php
+++ compras.php
@@ -1,6 +1,13 @@
 <?php
 
 require_once('twig_carregar.php');
+require('inc/banco.php');
+
+//Busca compras no banco
+$dados = $pdo->query('SELECT * FROM compras');
+$comp = $dados->fetchAll(PDO::FETCH_ASSOC);
+
 echo $twig->render('compras.html', [
-    'titulo' => 'Compras'
+    'titulo' => 'Compras',
+    'compras' => $comp
 ]);
